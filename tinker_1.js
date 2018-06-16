a580049db8578ac395b215c68c1ab0f735e5b30c
Matt Tilley <mtilley255@gmail.com>
2018-06-16 11:38:52 -0500

    End of session save of 'warmup' code.

--- tinker_1.js
+++ tinker_1.js
@@ -1,2 +1,36 @@
-let course_name = prompt("Enter name of the course you wish to take: ");
+
+
+
+
+
+
+/*
+let courses = [];
+
+function getInfo()
+{
+
+    let course_name = prompt("Enter name of the course you wish to take: ");
+
+    let instructor_name = prompt("Enter name of the instructor: ");
+
+    document.getElementById("st_1").innerHTML = ("You want to take the " + course_name + " course with professor " + instructor_name + ".");
+
+    if (course_name != "Underwater Basketweaving")
+        {
+            courses.push(course_name);
+        }
+    
+    
+    
+
+}
+
+
+console.log(courses);
+
+
+*/
+
+
 
