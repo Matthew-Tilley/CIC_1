c383ae4ae6f3eba961d7684e2ceab938c0e435ed
Matt Tilley <mtilley255@gmail.com>
2018-06-16 12:00:56 -0500

    Switching over to learning Vue / following tutorial.

--- tinker_1.js
+++ tinker_1.js
@@ -1,36 +1,8 @@
-
-
-
-
-
-
-/*
-let courses = [];
-
-function getInfo()
-{
-
-    let course_name = prompt("Enter name of the course you wish to take: ");
-
-    let instructor_name = prompt("Enter name of the instructor: ");
-
-    document.getElementById("st_1").innerHTML = ("You want to take the " + course_name + " course with professor " + instructor_name + ".");
-
-    if (course_name != "Underwater Basketweaving")
-        {
-            courses.push(course_name);
-        }
+var app = new Vue({
     
+    el: "#app",
+    data: {
+        message: "Hello Vue!"
+    }
     
-    
-
-}
-
-
-console.log(courses);
-
-
-*/
-
-
-
+});
